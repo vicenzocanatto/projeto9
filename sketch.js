058c92d5a52c0b8900f00708e56918c87f8b2309
vicenzocanatto <93734804+vicenzocanatto@users.noreply.github.com>
2021-11-04 16:02:07 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,24 +13,22 @@ function draw() {
    background(30);
    
    if (keyIsDown(RIGHT_ARROW)){
-    box.position.x = box.position.x +5;
+    background("red");
   }
 
     if (keyIsDown(LEFT_ARROW)){
-    box.position.x= box.position.x -5;
+      background("green");
   }
  
     if (keyIsDown(UP_ARROW)){
-    box.position.y = box.position.y - 5;
+      background("yellow");
   }
 
   if (keyIsDown(DOWN_ARROW)){
-    box.position.y = box.position.y + 5;
+    background("blue");
   }
 
   drawSprites();
 }
-
-
 
 
